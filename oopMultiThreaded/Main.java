357d02b7083a635aa59af708df8c4df8c8dc8bee
Sir4Token <147176071+Sir4Token@users.noreply.github.com>
2023-11-08 14:17:19 +0200

    Update Main.java

--- oopMultiThreaded/Main.java
+++ oopMultiThreaded/Main.java
@@ -2,10 +2,19 @@ package oopMultiThreaded;
 
 public class Main {
 	public static void main(String [] args) {
-
-			FactorialTask task1 = new FactorialTask(0);
-			task1.factorailstream(100);
-			
+		
+		 Thread[] td1 = new Thread[100];
+	        for (int i = 0; i < td1.length; i++) {
+	        	td1[i] = new Thread(new FactorialTask(i));
+	        }
+		
+	        for (int i = 0; i < td1.length; i++){
+	        	td1[i].start();
+	            try{
+	            	td1[i].join();
+	            }catch (InterruptedException e) {   
+	            }
+	        }
 	}
 }
 
